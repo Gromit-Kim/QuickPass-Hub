834fd37f0d98ab7a4747f1cb9a74f162c43c57dd
KSY <sye.g112@gmail.com>
2025-09-01 16:16:14 +0900

    init

---
+++ backend/src/main/kotlin/dev/syk/quickpasshub/QuickPass_Hub/QuickPassHubApplication.kt
@@ -0,0 +1,11 @@
+package dev.syk.quickpasshub.QuickPass_Hub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuickPassHubApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuickPassHubApplication>(*args)
+}
---
+++ backend/src/test/kotlin/dev/syk/quickpasshub/QuickPass_Hub/QuickPassHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.syk.quickpasshub.QuickPass_Hub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuickPassHubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
