d3c0a3537139dc8dc0056a4988bc022f8f4e638d
KSY <sye.g112@gmail.com>
2025-09-06 23:52:16 +0900

    feat(auth): authService에 회원가입 로직 구현

--- backend/src/main/kotlin/dev/syk/quickpasshub/QuickPass_Hub/auth/AuthService.kt
+++ backend/src/main/kotlin/dev/syk/quickpasshub/QuickPass_Hub/auth/AuthService.kt
@@ -1,8 +1,12 @@
 package dev.syk.quickpasshub.QuickPass_Hub.auth
 
+import dev.syk.quickpasshub.QuickPass_Hub.auth.dto.RegisterInput
 import dev.syk.quickpasshub.QuickPass_Hub.users.UsersRepository
+import dev.syk.quickpasshub.QuickPass_Hub.users.entities.UsersModel
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AuthService(
@@ -26,6 +30,31 @@ class AuthService(
 
         val accessToken: String = tokenService.createAccessToken(user.email, roles = listOf(user.role.name))
         val refreshToken: String = tokenService.createRefreshToken(user.email)
+
+        return Tokens(accessToken, refreshToken)
+    }
+
+    @Transactional
+    fun register(input: RegisterInput) : Tokens {
+        val exists : Boolean = usersRepository.existsByEmail(input.email)
+        if(exists){
+            throw IllegalArgumentException("이미 가입된 이메일입니다.")
+        }
+
+        // 2. 이메일 형식 검증/실존 확인은 개발 단계에선 생략 가능
+
+        // 운영시에는 클라이언트가 role을 "ADMIN"으로 보내지 못하도록 서버에서 강제 USER 지정을 권장함.
+        val passwordHash : String = passwordEncoder.encode(input.password)
+
+        val savedUser : UsersModel = try{
+            usersRepository.save(UsersModel(input.name, input.email, passwordHash, input.role))
+        }catch (e: DataIntegrityViolationException){
+            // 동시성으로 인해 유니크 제약 위반이 터질 수 있으니 보강
+            throw IllegalArgumentException("흠...")
+        }
+
+        val accessToken: String = tokenService.createAccessToken(savedUser.email, listOf(savedUser.role.name))
+        val refreshToken: String = tokenService.createRefreshToken(savedUser.email)
 
         return Tokens(accessToken, refreshToken)
     }
