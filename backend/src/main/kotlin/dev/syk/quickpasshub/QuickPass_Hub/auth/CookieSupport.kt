e3cd1dfa1c0dda3efe0de2fb7834f2c71ef596d4
KSY <sye.g112@gmail.com>
2025-09-06 22:11:16 +0900

    feat(auth): refresh token을 cookie로 관리하기 위한 보조 서비스 빈

---
+++ backend/src/main/kotlin/dev/syk/quickpasshub/QuickPass_Hub/auth/CookieSupport.kt
@@ -0,0 +1,40 @@
+package dev.syk.quickpasshub.QuickPass_Hub.auth
+
+import org.springframework.http.HttpCookie
+import org.springframework.http.ResponseCookie
+import org.springframework.stereotype.Component
+import java.time.Duration
+
+@Component
+class CookieSupport {
+
+    fun writeRefreshTokenCookie(
+        token: String,
+        secure: Boolean = false, // HTTPS라면 secure=true
+        sameSite: String = "Lax",
+        path: String = "/",
+        maxAge: Duration = Duration.ofDays(14)
+    ) : HttpCookie =
+        ResponseCookie
+            .from("refresh", token)
+            .httpOnly(true)
+            .secure(secure)
+            .sameSite(sameSite)
+            .path(path)
+            .maxAge(maxAge)
+            .build()
+
+    fun clearRefreshTokenCookie(
+        secure: Boolean = false,
+        sameSite : String = "Lax",
+        path: String = "/",
+    ) : HttpCookie =
+        ResponseCookie
+            .from("refresh", "")
+            .httpOnly(true)
+            .secure(secure)
+            .sameSite(sameSite)
+            .path(path)
+            .maxAge(0) // 삭제
+            .build()
+}
