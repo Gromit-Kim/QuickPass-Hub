674583211817d38a7ac99ea41a6710c0a536c182
KSY <sye.g112@gmail.com>
2025-09-07 22:42:19 +0900

    feat(SecurityConfig): 필터 체인 가이드 코드

--- backend/src/main/kotlin/dev/syk/quickpasshub/QuickPass_Hub/common/SecurityConfig.kt
+++ backend/src/main/kotlin/dev/syk/quickpasshub/QuickPass_Hub/common/SecurityConfig.kt
@@ -2,11 +2,32 @@ package dev.syk.quickpasshub.QuickPass_Hub.common
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig{
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain{
+        http
+            .csrf{
+                it.disable()
+            }
+            .authorizeHttpRequests {
+                it.requestMatchers(HttpMethod.POST, "/graphql").permitAll()
+                it.anyRequest().authenticated()
+            }
+            // .addFilterBefore()
+            .httpBasic {  it.disable() }
+            .formLogin {  it.disable() }
+        return http.build()
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
